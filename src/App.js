c57930b6b60190ef96356de76cba602898315841
Emeka Ewelike <emekaewelike@gmail.com>
2021-09-07 12:41:04 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch} from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import SectionAll from './components/SectionAll';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <section className="body">
+        <Navbar/>
+        <SectionAll/>
+        <Switch>
+          <Router exact path="/">
+            <Home />
+          </Router>
+          <Router exact path="/Contact">
+            <Contact />
+          </Router>
+          <Router exact path="/About">
+            <About />
+          </Router>
+        </Switch>
+      </section>
+      <Footer/>
+    </Router>
+    
   );
 }
 
 export default App;
+
+
+// https://preview.colorlib.com/#jackco
+
+// <form action="mailto:myforms@mydomain.com" enctype="text/plain" >
